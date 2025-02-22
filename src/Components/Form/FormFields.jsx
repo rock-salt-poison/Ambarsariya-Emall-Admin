eafa7df56fae70e08c5764b4cb8752b3969621ec
rock-salt-poison <rock.salt.poison@gmail.com>
2025-02-22 15:45:30 +0530

    Updates

--- src/Components/Form/FormFields.jsx
+++ src/Components/Form/FormFields.jsx
@@ -270,7 +270,7 @@ export default function FormFields({
           type='file'
           size="small"
           className={optionalCname}
-          required={required && !file}
+          required={required }
         />
         ) : type ? (
         <TextField
